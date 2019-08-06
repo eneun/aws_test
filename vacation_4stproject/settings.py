7b6d5f67526c20bd5ea99ecda2ec55ea114394c9
eneun <wtcat@naver.com>
2019-08-06 20:00:25 +0900

    commit

--- vacation_4stproject/settings.py
+++ vacation_4stproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qzs8%vvaj34e16rxd%3wy%r&62oqmt7tgzutykb$nc^3pelh9+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.f4pmuvf2ak.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
